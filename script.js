1ba43815572b3de7c23d97c9919a10c156886d54
diegomez-ang <diego.gomezfswd@gmail.com>
2020-11-24 23:04:13 -0500

    loteria first

---
+++ script.js
@@ -0,0 +1,164 @@
+var apuestas = [{ "nombre": "Sean", "apellido": "Fitzgerald", "edad": 59, "direccion": "Apartado núm.: 145, 239 Malesuada Carretera", "telefono": 3226261490, "apuesta": 107682 }, { "nombre": "Olga", "apellido": "David", "edad": 39, "direccion": "Apdo.:926-6038 Dui Carretera", "telefono": 3137578203, "apuesta": 247048 }, { "nombre": "Halla", "apellido": "Mann", "edad": 37, "direccion": "921-7782 Risus, C.", "telefono": 3121747588, "apuesta": 85889 }, { "nombre": "Jasper", "apellido": "Petty", "edad": 56, "direccion": "4118 Porttitor ", "telefono": 3129745056, "apuesta": 497107 }, { "nombre": "Lewis", "apellido": "Ashley", "edad": 19, "direccion": "Apdo.:255-2776 Pede Calle", "telefono": 3120876983, "apuesta": 161789 }, { "nombre": "Moana", "apellido": "Shepherd", "edad": 30, "direccion": "Apartado núm.: 213, 7497 Erat Carretera", "telefono": 3161880690, "apuesta": 529321 }, { "nombre": "Ori", "apellido": "Phelps", "edad": 56, "direccion": "Apdo.:549-4454 Phasellus Calle", "telefono": 3113016727, "apuesta": 328066 }, { "nombre": "Zeph", "apellido": "Snider", "edad": 31, "direccion": "9920 Nulla. ", "telefono": 3180625326, "apuesta": 541095 }, { "nombre": "Glenna", "apellido": "Guerrero", "edad": 56, "direccion": "605-6758 Risus ", "telefono": 3110801198, "apuesta": 160061 }, { "nombre": "Kathleen", "apellido": "Webster", "edad": 43, "direccion": "299-986 Urna. Avenida", "telefono": 3127464934, "apuesta": 181128 }, { "nombre": "Yvonne", "apellido": "Steele", "edad": 37, "direccion": "5004 Quis Avenida", "telefono": 3156694544, "apuesta": 923468 }, { "nombre": "Honorato", "apellido": "Schultz", "edad": 60, "direccion": "Apartado núm.: 570, 3350 Urna. C.", "telefono": 3156592843, "apuesta": 721028 }, { "nombre": "Zahir", "apellido": "Mcdaniel", "edad": 53, "direccion": "185-9663 Id Carretera", "telefono": 3148321293, "apuesta": 507686 }, { "nombre": "Warren", "apellido": "Allison", "edad": 29, "direccion": "Apdo.:444-5623 Sed, ", "telefono": 3129382085, "apuesta": 744499 }, { "nombre": "Jameson", "apellido": "Lynch", "edad": 36, "direccion": "Apdo.:441-430 Donec C/", "telefono": 3227627222, "apuesta": 799740 }, { "nombre": "Meredith", "apellido": "Moore", "edad": 25, "direccion": "3466 Cursus Avda.", "telefono": 3226045744, "apuesta": 936221 }, { "nombre": "Dara", "apellido": "Randolph", "edad": 57, "direccion": "Apdo.:636-7511 Nec C/", "telefono": 3188505773, "apuesta": 738454 }, { "nombre": "Jasper", "apellido": "Mckay", "edad": 62, "direccion": "893-5949 Orci. Carretera", "telefono": 3211881858, "apuesta": 860006 }, { "nombre": "Asher", "apellido": "Walter", "edad": 45, "direccion": "6908 Ullamcorper, Avenida", "telefono": 3199522342, "apuesta": 95600 }, { "nombre": "Lee", "apellido": "Cash", "edad": 32, "direccion": "481-3308 Nascetur ", "telefono": 3161474437, "apuesta": 385196 }, { "nombre": "Kirk", "apellido": "Mccormick", "edad": 65, "direccion": "Apdo.:201-9784 Ipsum Calle", "telefono": 3195328658, "apuesta": 160954 }, { "nombre": "Hayden", "apellido": "Mooney", "edad": 47, "direccion": "2190 Ullamcorper Carretera", "telefono": 3168939474, "apuesta": 990136 }, { "nombre": "Lael", "apellido": "Shepard", "edad": 54, "direccion": "8730 At C.", "telefono": 3224762919, "apuesta": 550324 }, { "nombre": "Kimberley", "apellido": "Harrington", "edad": 60, "direccion": "182-4079 Semper Avenida", "telefono": 3190786663, "apuesta": 155839 }, { "nombre": "Whilemina", "apellido": "Rhodes", "edad": 38, "direccion": "Apdo.:750-9966 Tristique C/", "telefono": 3185635446, "apuesta": 49052 }, { "nombre": "Madonna", "apellido": "Mcknight", "edad": 36, "direccion": "Apdo.:837-1642 Neque. C.", "telefono": 3111062657, "apuesta": 690996 }, { "nombre": "Jack", "apellido": "Hendrix", "edad": 26, "direccion": "1888 Molestie C.", "telefono": 3205588829, "apuesta": 635460 }, { "nombre": "Xerxes", "apellido": "Bernard", "edad": 42, "direccion": "Apdo.:412-2065 Duis Avenida", "telefono": 3203790552, "apuesta": 933145 }, { "nombre": "Graiden", "apellido": "Cooper", "edad": 58, "direccion": "Apartado núm.: 240, 3722 Morbi Av.", "telefono": 3159206148, "apuesta": 812914 }, { "nombre": "Charity", "apellido": "Sampson", "edad": 22, "direccion": "270-8746 Quisque Av.", "telefono": 3210522113, "apuesta": 641085 }, { "nombre": "Samson", "apellido": "Parrish", "edad": 28, "direccion": "Apdo.:429-5759 Lacus. Ctra.", "telefono": 3141675324, "apuesta": 220633 }, { "nombre": "Zorita", "apellido": "Hubbard", "edad": 41, "direccion": "Apartado núm.: 185, 4276 Mauris Avenida", "telefono": 3161529268, "apuesta": 145705 }, { "nombre": "Reese", "apellido": "Ballard", "edad": 20, "direccion": "Apdo.:295-2769 Ridiculus Avda.", "telefono": 3193935105, "apuesta": 538869 }, { "nombre": "Preston", "apellido": "Caldwell", "edad": 29, "direccion": "5780 Sociosqu C/", "telefono": 3217870736, "apuesta": 128047 }, { "nombre": "Karleigh", "apellido": "Hodges", "edad": 49, "direccion": "8482 Elit. Av.", "telefono": 3168718603, "apuesta": 540531 }, { "nombre": "Dane", "apellido": "Castro", "edad": 25, "direccion": "420 Tellus. C.", "telefono": 3193510045, "apuesta": 198731 }, { "nombre": "Hector", "apellido": "Mcpherson", "edad": 50, "direccion": "2696 Nunc Calle", "telefono": 3179831653, "apuesta": 960766 }, { "nombre": "Lars", "apellido": "Hendrix", "edad": 58, "direccion": "384-6830 Nunc Calle", "telefono": 3204041175, "apuesta": 935378 }, { "nombre": "Ryder", "apellido": "Gould", "edad": 49, "direccion": "Apdo.:701-5817 Consectetuer Avenida", "telefono": 3125358589, "apuesta": 314050 }, { "nombre": "Chanda", "apellido": "Foreman", "edad": 49, "direccion": "Apartado núm.: 832, 9250 Lorem, Carretera", "telefono": 3215792519, "apuesta": 989268 }, { "nombre": "Melissa", "apellido": "Shepherd", "edad": 49, "direccion": "245-4228 Ipsum Calle", "telefono": 3221140331, "apuesta": 731407 }, { "nombre": "Kristen", "apellido": "Johnson", "edad": 18, "direccion": "Apdo.:512-1769 Ridiculus Carretera", "telefono": 3157522636, "apuesta": 281848 }, { "nombre": "Calista", "apellido": "Patton", "edad": 43, "direccion": "Apdo.:534-5715 Convallis Calle", "telefono": 3238498863, "apuesta": 158025 }, { "nombre": "Christian", "apellido": "Sullivan", "edad": 18, "direccion": "Apdo.:814-488 Lectus. C.", "telefono": 3201635819, "apuesta": 278732 }, { "nombre": "Ashton", "apellido": "Hyde", "edad": 56, "direccion": "3325 Ornare. Calle", "telefono": 3239289403, "apuesta": 364764 }, { "nombre": "Cathleen", "apellido": "Bender", "edad": 62, "direccion": "Apartado núm.: 230, 1826 Pede C.", "telefono": 3167530119, "apuesta": 643445 }, { "nombre": "Regina", "apellido": "Callahan", "edad": 26, "direccion": "Apartado núm.: 923, 1212 Mauris C/", "telefono": 3147450574, "apuesta": 94269 }, { "nombre": "Josephine", "apellido": "Goodwin", "edad": 45, "direccion": "Apartado núm.: 770, 8996 Consectetuer C/", "telefono": 3225850707, "apuesta": 619163 }, { "nombre": "Chadwick", "apellido": "Walter", "edad": 46, "direccion": "3168 Felis C.", "telefono": 3213312866, "apuesta": 188332 }, { "nombre": "Christopher", "apellido": "Mcdowell", "edad": 59, "direccion": "737-8009 Velit Av.", "telefono": 3143445222, "apuesta": 770238 }, { "nombre": "Rosalyn", "apellido": "Webb", "edad": 30, "direccion": "5647 Aliquam Ctra.", "telefono": 3211468499, "apuesta": 525719 }, { "nombre": "Yoko", "apellido": "Olsen", "edad": 38, "direccion": "246-7116 Mauris C.", "telefono": 3188484357, "apuesta": 276282 }, { "nombre": "Yoshio", "apellido": "Weaver", "edad": 35, "direccion": "9658 Amet C/", "telefono": 3152554655, "apuesta": 231539 }, { "nombre": "Lydia", "apellido": "Boyer", "edad": 40, "direccion": "Apdo.:797-9885 Pulvinar ", "telefono": 3191305948, "apuesta": 330287 }, { "nombre": "Derek", "apellido": "Chavez", "edad": 49, "direccion": "Apdo.:789-3172 Malesuada Calle", "telefono": 3157998316, "apuesta": 40869 }, { "nombre": "Abbot", "apellido": "Britt", "edad": 49, "direccion": "459-9562 Amet Carretera", "telefono": 3110680449, "apuesta": 924126 }, { "nombre": "Ira", "apellido": "Hall", "edad": 65, "direccion": "351-4152 Ut, C/", "telefono": 3132792577, "apuesta": 41353 }, { "nombre": "Keane", "apellido": "Frye", "edad": 64, "direccion": "2415 Augue Ctra.", "telefono": 3220672330, "apuesta": 58007 }, { "nombre": "Sigourney", "apellido": "Norris", "edad": 23, "direccion": "2088 Leo Calle", "telefono": 3129856392, "apuesta": 566724 }, { "nombre": "Uriel", "apellido": "Booker", "edad": 59, "direccion": "109-5974 Nulla Calle", "telefono": 3155084465, "apuesta": 851499 }, { "nombre": "Martha", "apellido": "Dotson", "edad": 48, "direccion": "889-6384 Erat C.", "telefono": 3221060191, "apuesta": 530252 }, { "nombre": "Yeo", "apellido": "Mullen", "edad": 53, "direccion": "Apartado núm.: 382, 893 Mollis C/", "telefono": 3152523438, "apuesta": 624442 }, { "nombre": "Tate", "apellido": "Stein", "edad": 18, "direccion": "Apdo.:730-446 Semper C/", "telefono": 3163039476, "apuesta": 928527 }, { "nombre": "Hamish", "apellido": "Conley", "edad": 42, "direccion": "5636 Natoque Av.", "telefono": 3100865984, "apuesta": 77363 }, { "nombre": "Quinlan", "apellido": "Carrillo", "edad": 51, "direccion": "290 Nisi ", "telefono": 3152645804, "apuesta": 784292 }, { "nombre": "Basil", "apellido": "Kane", "edad": 42, "direccion": "276-5992 Consectetuer Av.", "telefono": 3192481514, "apuesta": 546598 }, { "nombre": "Darryl", "apellido": "Mcguire", "edad": 44, "direccion": "Apdo.:743-1201 Scelerisque Av.", "telefono": 3203976034, "apuesta": 454419 }, { "nombre": "Blaze", "apellido": "Gibbs", "edad": 37, "direccion": "613-6945 Amet Avenida", "telefono": 3213006169, "apuesta": 605730 }, { "nombre": "Louis", "apellido": "Terrell", "edad": 44, "direccion": "Apartado núm.: 544, 4683 Aliquam Calle", "telefono": 3113950835, "apuesta": 184105 }, { "nombre": "Rama", "apellido": "Matthews", "edad": 39, "direccion": "810-8715 Duis Av.", "telefono": 3117856054, "apuesta": 471346 }, { "nombre": "Dean", "apellido": "Richardson", "edad": 47, "direccion": "589-5622 Amet Avenida", "telefono": 3236056510, "apuesta": 881947 }, { "nombre": "Dacey", "apellido": "Mckinney", "edad": 46, "direccion": "Apartado núm.: 135, 5117 Montes, Calle", "telefono": 3157681778, "apuesta": 27846 }, { "nombre": "Kasper", "apellido": "Johns", "edad": 59, "direccion": "Apartado núm.: 256, 7234 Arcu. C/", "telefono": 3222166768, "apuesta": 759471 }, { "nombre": "Dexter", "apellido": "Webb", "edad": 27, "direccion": "9693 Blandit Carretera", "telefono": 3183859308, "apuesta": 941897 }, { "nombre": "Quamar", "apellido": "Guzman", "edad": 32, "direccion": "915-2920 Aliquet Avenida", "telefono": 3150299451, "apuesta": 338132 }, { "nombre": "Kitra", "apellido": "Mckenzie", "edad": 48, "direccion": "1757 Duis C/", "telefono": 3166333904, "apuesta": 454418 }, { "nombre": "Jemima", "apellido": "Gaines", "edad": 53, "direccion": "Apartado núm.: 694, 3036 Nunc C/", "telefono": 3157471543, "apuesta": 964277 }, { "nombre": "Macaulay", "apellido": "Palmer", "edad": 35, "direccion": "Apartado núm.: 666, 8718 Consectetuer Avenida", "telefono": 3168768204, "apuesta": 428751 }, { "nombre": "Constance", "apellido": "Gamble", "edad": 32, "direccion": "689-4526 Suspendisse Calle", "telefono": 3120886730, "apuesta": 457599 }, { "nombre": "Hector", "apellido": "Craft", "edad": 32, "direccion": "600-9992 Eu, Avenida", "telefono": 3232279251, "apuesta": 416400 }, { "nombre": "Brady", "apellido": "Delgado", "edad": 31, "direccion": "Apartado núm.: 823, 8992 Nisi. Av.", "telefono": 3217941375, "apuesta": 193375 }, { "nombre": "Catherine", "apellido": "Rosario", "edad": 19, "direccion": "649 Ullamcorper, Avda.", "telefono": 3201470098, "apuesta": 629222 }, { "nombre": "Keith", "apellido": "Oneil", "edad": 39, "direccion": "Apdo.:679-8548 Rutrum Avenida", "telefono": 3172532845, "apuesta": 473216 }, { "nombre": "Ulric", "apellido": "Dotson", "edad": 62, "direccion": "Apdo.:478-7778 Arcu Calle", "telefono": 3141353995, "apuesta": 738408 }, { "nombre": "Micah", "apellido": "Moss", "edad": 55, "direccion": "Apdo.:738-6065 Urna. Calle", "telefono": 3198666420, "apuesta": 490117 }, { "nombre": "Jesse", "apellido": "Lopez", "edad": 37, "direccion": "Apartado núm.: 968, 1559 Fringilla C.", "telefono": 3146114016, "apuesta": 993455 }, { "nombre": "Hamish", "apellido": "Harrell", "edad": 48, "direccion": "813-1851 Ut ", "telefono": 3100952429, "apuesta": 549344 }, { "nombre": "Marny", "apellido": "Fletcher", "edad": 60, "direccion": "Apartado núm.: 812, 2077 Enim. C.", "telefono": 3148781603, "apuesta": 536859 }, { "nombre": "Harriet", "apellido": "Mcmahon", "edad": 39, "direccion": "791-533 Cursus Calle", "telefono": 3220347539, "apuesta": 96974 }, { "nombre": "Nissim", "apellido": "Peters", "edad": 45, "direccion": "4064 Ultricies Ctra.", "telefono": 3104902185, "apuesta": 401667 }, { "nombre": "Cassidy", "apellido": "Parsons", "edad": 35, "direccion": "8955 Tristique C/", "telefono": 3213604223, "apuesta": 41825 }, { "nombre": "Mollie", "apellido": "King", "edad": 41, "direccion": "Apartado núm.: 697, 3828 Vitae C.", "telefono": 3102123299, "apuesta": 353129 }, { "nombre": "Kelly", "apellido": "Malone", "edad": 23, "direccion": "Apartado núm.: 714, 724 Ipsum C/", "telefono": 3128264229, "apuesta": 121742 }, { "nombre": "Priscilla", "apellido": "Ferrell", "edad": 19, "direccion": "Apartado núm.: 813, 6797 Dictum Avenida", "telefono": 3131656046, "apuesta": 96556 }, { "nombre": "Breanna", "apellido": "Haynes", "edad": 25, "direccion": "5494 Enim. Avenida", "telefono": 3116679881, "apuesta": 212698 }, { "nombre": "Jacob", "apellido": "Barlow", "edad": 63, "direccion": "Apdo.:925-6645 Elit, Carretera", "telefono": 3156420616, "apuesta": 98468 }, { "nombre": "Kimberly", "apellido": "Witt", "edad": 29, "direccion": "Apdo.:595-8450 Suspendisse Carretera", "telefono": 3173320100, "apuesta": 120346 }, { "nombre": "Ruth", "apellido": "Mathews", "edad": 26, "direccion": "170-5756 Arcu. Avda.", "telefono": 3159611746, "apuesta": 100152 }, { "nombre": "Nasim", "apellido": "Wilkerson", "edad": 46, "direccion": "400-1366 Nec, Ctra.", "telefono": 3155031466, "apuesta": 967461 }, { "nombre": "Freya", "apellido": "Kelley", "edad": 51, "direccion": "7423 Id, ", "telefono": 3195747915, "apuesta": 192178 }];
+
+function newCompra() {
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+    if (num1.value == num2.value) {
+        var n = document.getElementById("nombre");
+        var a = document.getElementById("apellido");
+        var e = document.getElementById("edad");
+        var d = document.getElementById("dir");
+        var t = document.getElementById("tel");
+        var prueba = document.getElementById("prueba");
+        var v = 0;
+        var datos = [n, a, e, d, t, num1, num2];
+        datos.forEach(k => {
+            if (k.value != '') {
+                v++;
+            }
+        });
+
+        if (v == 7) {
+            var nCompra = new Persona(n.value, a.value, e.value, d.value, t.value, num1.value);
+            apuestas.push(nCompra.getAllParams());
+            console.log("Total apuestas realizadas: ", apuestas);
+            datos.forEach(k => {
+                k.value = '';
+            });
+            alert("Se completó el registro correctamente.")
+        }
+        else {
+            alert("Faltan datos por completar. Asegúrese de llenar todos los controles para continuar");
+        }
+
+    }
+    else {
+        alert("Los números ingresados no son iguales. Por favor verifique.");
+    }
+
+}
+
+function abc() {
+
+    var ganador = '497107'; //490117
+    var gExactos = [];
+    var y = 0;
+    apuestas.forEach(e => {
+        if (e.apuesta == ganador) {
+            prueba.innerText = e.nombre;
+            console.log("Nombre: ", e.nombre);
+            console.log("Apellido: ", e.apellido);
+            console.log("Edad: ", e.edad);
+            console.log("Direccion: ", e.direccion);
+            console.log("Telefono: ", e.telefono);
+            console.log("Numero apostado: ", e.apuesta);
+            console.log("-------------------");
+            gExactos.push(y);
+        }
+        y++;
+
+    });
+
+    console.log("\nBuscando ganadores de cuatro (4) dígitos\n");
+    var a = 0;
+    var g4Digitos = [];
+    apuestas.forEach(k => {
+
+        var count = 0;
+        for (let i = 0; i < 6; i++) {
+            if (k.apuesta.toString()[i] == ganador[i]) {
+                count++;
+            }
+        }
+
+        if (count >= 4 && count < 6) {
+            console.log("Nombre: ", k.nombre);
+            console.log("Apellido: ", k.apellido);
+            console.log("Edad: ", k.edad);
+            console.log("Direccion: ", k.direccion);
+            console.log("Telefono: ", k.telefono);
+            console.log("Numero apostado: ", k.apuesta);
+            console.log("-------------------");
+            g4Digitos.push(a);
+        }
+        a++;
+    });
+
+
+    console.log("\nBuscando ganadores del 10% por estar cerca a los ganadores\n");
+    // Unir los dos Arrays
+    var indices = gExactos.concat(g4Digitos);
+    console.log(indices)
+
+    var f = 0;
+    indices.forEach(k => {
+        let pAntes = apuestas[(k-1)];
+        console.log("Nombre: ", pAntes.nombre);
+        console.log("Apellido: ", pAntes.apellido);
+        console.log("Edad: ", pAntes.edad);
+        console.log("Direccion: ", pAntes.direccion);
+        console.log("Telefono: ", pAntes.telefono);
+        console.log("Numero apostado: ", pAntes.apuesta);
+        console.log("-------------------");
+
+        let pDespues = apuestas[(k+1)];
+        console.log("Nombre: ", pDespues.nombre);
+        console.log("Apellido: ", pDespues.apellido);
+        console.log("Edad: ", pDespues.edad);
+        console.log("Direccion: ", pDespues.direccion);
+        console.log("Telefono: ", pDespues.telefono);
+        console.log("Numero apostado: ", pDespues.apuesta);
+        console.log("-------------------");
+    });
+
+
+}
+
+
+class Persona {
+    constructor(nombre, apellido, edad, direcccion, telefono, apuesta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direcccion;
+        this.telefono = telefono;
+        this.apuesta = apuesta;
+    }
+
+    getTel() {
+        return this.telefono;
+    }
+
+    getDir() {
+        return this.direccion;
+    }
+
+    getFullName() {
+        return this.nombre + " " + this.apellido;
+    }
+
+    getEdad() {
+        return this.edad;
+    }
+
+    getApuesta() {
+        return this.apuesta;
+    }
+
+    getAllParams() {
+        var allParams = {
+            nombre: this.nombre,
+            apellido: this.apellido,
+            telefono: this.telefono,
+            edad: this.edad,
+            direccion: this.direccion,
+            apuesta: this.apuesta
+        }
+        return allParams;
+    }
+
+    setGanador(tipo) {
+        this.tipoGanancia = tipo;
+    }
+
+}
